4f3a2b96702eb061a47ea87098b11051409b60d0
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-10-25 14:14:49 -0400

    added randomTime

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
 let on = false
 const start = document.querySelector('.start')
-
-
 const holes = document.querySelectorAll('.hole')
+const controls = document.querySelector('.controls')
+const moles = document.querySelector('.mole')
 
-// holes.addEventListener('click', popUp)
-start.addEventListener('click', startGame)
 
-// function popUp = (holes) {
-  holes.forEach(hole => {
-    hole.addEventListener('click', popUp)
-  })
+start.addEventListener('click', (event) => {
+  if (start) {
+    on = true;
+	} else {
+    on = false;
+    controls.classList.remove('show')
+    // clear all data, reset timer, lives, in controls class
+	}
+	if (on || win) {
+    startGame();
+	}
+});
 
-// }
-function popUp(e) {
-  console.log('popUp')
-  console.log(e.target)
-  e.target.classList.add('mole.up')
+function startGame() {
+  win = false;
+ controls.classList.add('show')
+  // start timer, all stuff in controls class
+  on = true
+  moleHit()
+  popup()
+}
+
+
+
+holes.forEach(hole => {
+  hole.addEventListener('click', moleHit)
+})
+
+function moleHit(e) {
+  const hole = e.target.dataset.id
+  console.log('hole', hole)
+  // will add to score
+  // minus mole, say or look hit
+}
+
+function popup() {
+  const time = randomTime(1000, 500)
+  const hole = randomHole(holes)
+  console.log(hole)
 
 }
+
+function randomTime(max, min) {
+  return Math.random() * (max - min) + min;
+}
+
+
+
