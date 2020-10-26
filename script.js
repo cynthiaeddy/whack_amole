becf8bcae0554d7a38c6f438cf04033a8aac297c
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-10-26 09:51:01 -0400

    works with bug

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-let on = false
 const start = document.querySelector('.start')
 const holes = document.querySelectorAll('.hole')
 const controls = document.querySelector('.controls')
-const moles = document.querySelector('.mole')
+const moles = document.querySelectorAll('.mole')
+let score = document.querySelector('.score')
+let molesRemaining = document.querySelector('.moles-remaining')
 
+score = 0
+let on = false
+let lastHole
+let lastMole
+let timeUp = false
+let moleLives = 5
 
 start.addEventListener('click', (event) => {
   if (start) {
     on = true;
-	} else {
-    on = false;
-    controls.classList.remove('show')
-    // clear all data, reset timer, lives, in controls class
-	}
+  }
+  // else {
+  //   on = false;
+  //   controls.classList.remove('show')
+  //   // clear all data, reset timer, lives, in controls class
+	// }
 	if (on || win) {
     startGame();
 	}
@@ -22,9 +30,11 @@ function startGame() {
   win = false;
  controls.classList.add('show')
   // start timer, all stuff in controls class
+  // timeUp = false;
+  score = 0;
   on = true
-  moleHit()
   popup()
+  setTimeout(() => timeUp = true, 30000)
 }
 
 
@@ -34,21 +44,42 @@ holes.forEach(hole => {
 })
 
 function moleHit(e) {
-  const hole = e.target.dataset.id
-  console.log('hole', hole)
+  const holeHit = e.target.dataset.id
+  console.log('holeHit', holeHit, e.target.dataset.id)
+  if (holeHit) {
+    score++
+    console.log(score)
+  }
   // will add to score
   // minus mole, say or look hit
 }
 
 function popup() {
   const time = randomTime(1000, 500)
-  const hole = randomHole(holes)
-  console.log(hole)
-
+  mole = randomHole(moles)
+  mole.classList.add('up');
+    setTimeout(() => {
+      mole.classList.remove('up');
+      if(!timeUp) {
+        popup();
+      }
+    }, time);
 }
+
 
 function randomTime(max, min) {
   return Math.random() * (max - min) + min;
+}
+
+function randomHole(moles){
+  const index = Math.floor(Math.random() * 9 + 1);
+  console.log(index)
+  const mole = moles[index];
+    if (mole === lastMole){
+        return randomHole(moles);
+    }
+    lastMole = mole;
+    return mole;
 }
 
 
